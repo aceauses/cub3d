31b8b650c6662a68e8a426104b2a0e5a290088d1
aceauses <aceauses@student.42heilbronn.de>
2024-02-07 15:14:49 +0100

    Update/Bug Fixes
    ++ map_values_utils.c
      ^^ Separate some functions from map_values.c so we can be norm ok
      ^^ Introducing clean_compare(char *, char *, size_t) that is strtrim the beginning
        and the end of the specific string and after that its comparing and return 1 if the
         string is the same or 0 if its different.
      ^^ Can be used for future functions or maybe more checks
    
    !+ utils_1.c
     ^^ Implement array_length to calculate how big is a double pointer
    
    !+ map_values.c
     ^^ Change the way function work also implement some more checks for the textures
     ^^ Also implement some Description.
    
    !+ Makefile
      ^^ Add the compile of new files
    
    !+ cub3d.h
     ^^ add some more define for errors
    
    !+ map_validation_1.c
     ** FIXED SEGFAULT if the user didnt give a argument as segfault now fixed by verify if the argv == NULL or not
    
    !! Maybe we can add description to all functions
    && The program su

--- src/map_validations/map_values.c
+++ src/map_validations/map_values.c
@@ -6,23 +6,15 @@
 /*   By: aceauses <aceauses@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 18:09:41 by aceauses          #+#    #+#             */
-/*   Updated: 2024/02/05 20:23:32 by aceauses         ###   ########.fr       */
+/*   Updated: 2024/02/07 11:26:30 by aceauses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static int	dcte(char *buffer, char *split, int *check)
-{
-	if (ft_strnstr(buffer, split, ft_strlen(buffer)))
-	{
-		*check += 6;
-		return (1);
-	}
-	*check += 1;
-	return (0);
-}
-
+/**
+ * @brief Function to check if the textures are existing and correct
+*/
 static int	check_textures(char **map)
 {
 	int		i;
@@ -36,13 +28,12 @@ static int	check_textures(char **map)
 	while (map[++i] != NULL && check <= 4)
 	{
 		split = ft_split(map[i], ' ');
-		if ((!ft_strcmp(split[0], "NO") && !dcte(buffer, split[0], &check))
-			|| (!ft_strcmp(split[0], "SO") && !dcte(buffer, split[0], &check))
-			|| (!ft_strcmp(split[0], "WE") && !dcte(buffer, split[0], &check))
-			|| (!ft_strcmp(split[0], "EA") && !dcte(buffer, split[0], &check)))
+		if (array_length(split) == 2
+			&& should_check_path(split, buffer, &check))
 		{
 			if (open(split[1], O_RDONLY) == -1)
-				return (map_errors(PATH_ERROR), free(buffer), free_double_pointer(split), 0);
+				return (map_errors(PATH_ERROR)
+					, free(buffer), free_double_pointer(split), 0);
 			buffer = free_join(buffer, split[0]);
 		}
 		free_double_pointer(split);
@@ -52,6 +43,9 @@ static int	check_textures(char **map)
 	return (free(buffer), 1);
 }
 
+/**
+ * @brief Custom ft_isdigit function to check the hole string for digits
+*/
 static int	is_digit(char *str)
 {
 	int		i;
@@ -66,6 +60,9 @@ static int	is_digit(char *str)
 	return (1);
 }
 
+/**
+ * @brief Function to check if the RGB values of the Floor are correct
+*/
 static int	check_rgb_f(char **map)
 {
 	int		i;
@@ -75,11 +72,42 @@ static int	check_rgb_f(char **map)
 
 	i = 0;
 	x = -1;
-	while (map[i] && ft_strncmp(map[i], "F", 1) != 0)
+	while (map[i] && clean_compare(map[i], "F", 1) != 1)
 		i++;
 	split = ft_split(map[i], ' ');
 	if (!split)
-		return (0);
+		return (map_errors(CANNOT_FIND_F), 0);
+	values = ft_split(split[1], ',');
+	if (!values)
+		return (free_double_pointer(split), 0);
+	while (i != array_length(map) && values[++x] != NULL)
+		if (!is_digit(values[x])
+			|| ft_atoi(values[x]) > 255 || ft_atoi(values[x]) < 0)
+			return (map_errors(RGB_ERROR_2)
+				, free_double_pointer(values), free_double_pointer(split), 0);
+	if (x != 3)
+		return (map_errors(RGB_ERROR)
+			, free_double_pointer(values), free_double_pointer(split), 0);
+	return (free_double_pointer(values), free_double_pointer(split), 1);
+}
+
+/**
+ * @brief Function to check if the RGB values of the Ceiling are correct
+*/
+static int	check_rgb_c(char **map)
+{
+	int		i;
+	char	**split;
+	char	**values;
+	int		x;
+
+	i = 0;
+	x = -1;
+	while (map[i] && clean_compare(map[i], "C", 1) != 1)
+		i++;
+	split = ft_split(map[i], ' ');
+	if (!split)
+		return (map_errors(CANNOT_FIND_C), 0);
 	values = ft_split(split[1], ',');
 	if (!values)
 		return (free_double_pointer(split), 0);
@@ -94,33 +122,9 @@ static int	check_rgb_f(char **map)
 	return (free_double_pointer(values), free_double_pointer(split), 1);
 }
 
-static int	check_rgb_c(char **map)
-{
-	int		i;
-	char	**split;
-	char	**values;
-	int		x;
-
-	i = 0;
-	x = -1;
-	while (map[i] && ft_strncmp(map[i], "C", 1) != 0)
-		i++;
-	split = ft_split(map[i], ' ');
-	if (!split)
-		return (0);
-	values = ft_split(split[1], ',');
-	if (!values)
-		return (free_double_pointer(split), 0);
-	while (values[++x] != NULL)
-		if (!is_digit(values[x])
-			|| ft_atoi(values[x]) > 255 || ft_atoi(values[x]) < 0)
-			return (free_double_pointer(values), free_double_pointer(split), 0);
-	if (x != 3)
-		return (map_errors(RGB_ERROR)
-			, free_double_pointer(values), free_double_pointer(split), 0);
-	return (free_double_pointer(values), free_double_pointer(split), 1);
-}
-
+/**
+ * @brief Function to check if the map values are correct
+*/
 int	check_map_values(char **map)
 {
 	if (!map || !map[0])
--- src/map_validations/map_walls.c
+++ src/map_validations/map_walls.c
@@ -6,7 +6,7 @@
 /*   By: aceauses <aceauses@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 22:00:02 by aceauses          #+#    #+#             */
-/*   Updated: 2024/02/05 20:23:49 by aceauses         ###   ########.fr       */
+/*   Updated: 2024/02/07 15:03:28 by aceauses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ static int	find_first_wall(char **map, int *x, int *y)
 	while (map && map[y_m] != NULL)
 	{
 		x_m = 0;
+		while (map[y_m][x_m] == ' ' && (map[y_m][x_m] != '1' && map[y_m][x_m] != '\0'))
+			x_m++;
 		if (!ft_strncmp(map[y_m], "SO", 2) || !ft_strncmp(map[y_m], "NO", 2)
 			|| !ft_strncmp(map[y_m], "WE", 2) || !ft_strncmp(map[y_m], "EA", 2)
 			|| !ft_strncmp(map[y_m], "F", 1) || !ft_strncmp(map[y_m], "C", 1))
@@ -28,8 +30,6 @@ static int	find_first_wall(char **map, int *x, int *y)
 				y_m++;
 				continue;
 			}
-		while (map[y_m][x_m] == ' ' || (map[y_m][x_m] != '1' && map[y_m][x_m] != '\0'))
-			x_m++;
 		if (map[y_m][x_m] == '1')
 		{
 			*x = x_m;
@@ -87,10 +87,9 @@ int	check_walls(char **map)
 	y = 0;
 	while (find_first_wall(map, &x, &y) == 1)
 	{
-		printf("%d %d, %c\n", y, x, map[y][x]);
 		fill(map, y, x);
 	}
-	// find_first_wall(map, &x, &y);
-	// fill(map, y, x);
+	for (int i = 0; map[i] != NULL; i++)
+		printf("%s\n", map[i]);
 	return (1);
 }
